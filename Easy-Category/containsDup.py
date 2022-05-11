8c860a08fb71ed42c5228114b553994270561e59
HarmanSMann <76751076+HarmanSMann@users.noreply.github.com>
2022-05-11 14:39:55 -0400

    Create containsDup.py

---
+++ Easy-Category/containsDup.py
@@ -0,0 +1,14 @@
+# Contain dupes: https://leetcode.com/problems/contains-duplicate/
+
+# one way to solve the problem is to iterate through the array and store all 
+# unique values into a set, if something is already in the set, then stop checking and return contains dup
+
+example = [1,2,3,4,5,6,6,7,8,10]
+
+dup = {}
+
+for n in example:
+  if number in dup:
+    return True
+  else:
+    dup[number] = 1
